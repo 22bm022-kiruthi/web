437563b5e4ae84ad134441b03526dc4991254475
22bm022-kiruthi <22bm022@drngpit.ac.in>
2026-04-15 15:50:33 +0530

    fix render deploy

--- src/components/app.py
+++ src/components/app.py
@@ -4,7 +4,13 @@ from firebase_admin import credentials, firestore
 import os
 
 app = Flask(__name__)
+@app.route("/")
+def home():
+    return "Render Deploy Working!"
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 # Connect Firebase
 
 
